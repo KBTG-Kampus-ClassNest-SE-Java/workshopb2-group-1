fdca4c0631279c47e1011e34565a551ed7efe9f7
Goat <118417622+goatshub@users.noreply.github.com>
2024-03-30 17:40:05 +0700

    add SR-7 applyCartPromotion

--- kbazaar/src/main/java/com/kampus/kbazaar/cart/CartController.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/CartController.java
@@ -1,5 +1,6 @@
 package com.kampus.kbazaar.cart;
 
+import com.kampus.kbazaar.promotion.PromotionApplyCartRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,8 +8,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 public class CartController {
 
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
     @GetMapping("/carts")
     public ResponseEntity getCart() { // NOSONAR
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/carts/{username}/promotions")
+    public CartResponse applyCartPromotion(@PathVariable String username, @RequestBody PromotionApplyCartRequest promotionApplyCartRequest) {
+        return cartService.applyCartPromotion(username, promotionApplyCartRequest);
     }
 }
---
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/CartService.java
@@ -0,0 +1,14 @@
+package com.kampus.kbazaar.cart;
+
+import com.kampus.kbazaar.promotion.PromotionApplyCartRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+    public CartResponse applyCartPromotion(String username, PromotionApplyCartRequest promotionApplyCartRequest) {
+        return new CartResponse();
+    }
+    
+}
