d7a36423e8f2525c1b4c97a47ab72b77f58cd00d
Apirom Na Nakorn <apirom9@gmail.com>
2024-03-30 21:56:14 +0700

    Fix fail test DependencyRuleTest

--- kbazaar/src/main/java/com/kampus/kbazaar/cart/CartItemRepository.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/CartItemRepository.java
@@ -1,9 +1,11 @@
 package com.kampus.kbazaar.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     List<CartItem> findByUsername(String username);
 }
