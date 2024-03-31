c384fdb91f569d7793648e0cdb8857c3558b2ad7
WorabordinC <athiphat846@gmail.com>
2024-03-31 13:11:38 +0700

    clean comment

--- kbazaar/src/main/java/com/kampus/kbazaar/cart/CartService.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/CartService.java
@@ -1,6 +1,5 @@
 package com.kampus.kbazaar.cart;
 
-import com.kampus.kbazaar.cartitem.CartItemRepository;
 import com.kampus.kbazaar.cartitem.CartItemService;
 import com.kampus.kbazaar.promotion.PromotionApplyCartRequest;
 import com.kampus.kbazaar.shopper.Shopper;
@@ -11,8 +10,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class CartService {
 
-    private final CartRepository cartRepository;
-    private final CartItemRepository cartItemRepository;
     private final CartItemService cartItemService;
 
     private final ShopperRepository shopperRepository;
@@ -22,13 +19,7 @@ public class CartService {
         return new CartResponse();
     }
 
-    public CartService(
-            CartRepository cartRepository,
-            CartItemRepository cartItemRepository,
-            ShopperRepository shopperRepository,
-            CartItemService cartItemService) {
-        this.cartRepository = cartRepository;
-        this.cartItemRepository = cartItemRepository;
+    public CartService(ShopperRepository shopperRepository, CartItemService cartItemService) {
         this.shopperRepository = shopperRepository;
         this.cartItemService = cartItemService;
     }
@@ -40,41 +31,8 @@ public class CartService {
         List<String> usernameList =
                 shopperRepository.findAll().stream().map(Shopper::getUsername).toList();
 
-        // for loop getcartby usernam
-        List<CartResponse> carts =
-                usernameList.stream().map(cartItemService::getCartByUsername).toList();
+        // for loop get cart by username
 
-        return carts;
+        return usernameList.stream().map(cartItemService::getCartByUsername).toList();
     }
-
-    //    public List<CartResponse> getAllCart() {
-    //        List<Cart> carts = cartRepository.findAll();
-    //        List<CartResponse> cartResponseDto = new ArrayList<>();
-    //        for (Cart cart : carts) {
-    //            List<CartItem> cartItems = cartItemRepository.findByUsername(cart.getUsername());
-    //            List<CartItemDto> cartItemDtos = new ArrayList<>();
-    //            for (CartItem cartItem : cartItems) {
-    //                cartItemDtos.add(
-    //                        new CartItemDto(
-    //                                cartItem.getId(),
-    //                                cartItem.getUsername(),
-    //                                cartItem.getSku(),
-    //                                cartItem.getPrice(),
-    //                                cartItem.getQuantity(),
-    //                                cartItem.getDiscount(),
-    //                                cartItem.getPromotionCodes()));
-    //            }
-    //
-    //            CartResponseDto cartResponse =
-    //                    new CartResponseDto(
-    //                            cart.getUsername(),
-    //                            cartItemDtos,
-    //                            cart.getDiscount(),
-    //                            cart.getTotalDiscount(),
-    //                            cart.getSubtotal(),
-    //                            cart.getGrandTotal());
-    //        }
-    //
-    //        return cartResponseDto;
-    //    }
 }
