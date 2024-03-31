8a5b2f50e7308b35966b5208906fbec84252d565
Apirom Na Nakorn <apirom9@gmail.com>
2024-03-31 11:35:37 +0700

    Format code

--- kbazaar/src/test/java/com/kampus/kbazaar/cartitem/CartItemServiceTest.java
+++ kbazaar/src/test/java/com/kampus/kbazaar/cartitem/CartItemServiceTest.java
@@ -1,49 +1,45 @@
 package com.kampus.kbazaar.cartitem;
 
-import com.kampus.kbazaar.cart.Cart;
-import com.kampus.kbazaar.cart.CartRepository;
-import com.kampus.kbazaar.cart.CartResponse;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.kampus.kbazaar.cart.Cart;
+import com.kampus.kbazaar.cart.CartRepository;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 @ExtendWith(MockitoExtension.class)
 class CartItemServiceTest {
 
-    @InjectMocks
-    private CartItemService cartItemService;
+    @InjectMocks private CartItemService cartItemService;
 
-    @Mock
-    private CartItemRepository cartItemRepository;
+    @Mock private CartItemRepository cartItemRepository;
 
-    @Mock
-    private CartRepository cartRepository;
+    @Mock private CartRepository cartRepository;
 
     @Test
     void shouldAddCartItem() {
-        CartItem cartItem = CartItem.builder()
-                .id(1L)
-                .username("username")
-                .sku("sku")
-                .name("name")
-                .price(100.0)
-                .quantity(1)
-                .discount(0)
-                .promotionCodes("promo-code")
-                .build();
+        CartItem cartItem =
+                CartItem.builder()
+                        .id(1L)
+                        .username("username")
+                        .sku("sku")
+                        .name("name")
+                        .price(100.0)
+                        .quantity(1)
+                        .discount(0)
+                        .promotionCodes("promo-code")
+                        .build();
 
         Cart cart = new Cart();
         cart.setId(1L);
