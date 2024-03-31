a56501d54ed5527698870c1ccb8f6316ec7b1279
WorabordinC <athiphat846@gmail.com>
2024-03-31 13:03:15 +0700

    add code coverage CartItemService

--- kbazaar/src/test/java/com/kampus/kbazaar/cartitem/CartItemServiceTest.java
+++ kbazaar/src/test/java/com/kampus/kbazaar/cartitem/CartItemServiceTest.java
@@ -1,30 +1,33 @@
 package com.kampus.kbazaar.cartitem;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import com.kampus.kbazaar.cart.Cart;
 import com.kampus.kbazaar.cart.CartRepository;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class CartItemServiceTest {
 
-    @InjectMocks private CartItemService cartItemService;
+    @InjectMocks
+    private CartItemService cartItemService;
 
-    @Mock private CartItemRepository cartItemRepository;
+    @Mock
+    private CartItemRepository cartItemRepository;
 
-    @Mock private CartRepository cartRepository;
+    @Mock
+    private CartRepository cartRepository;
 
     @Test
     void shouldAddCartItem() {
@@ -56,5 +59,23 @@ class CartItemServiceTest {
         cartItemService.addCartItem(cartItem);
 
         verify(cartItemRepository).save(any());
+    }
+
+    @Test
+    void shouldGetCartByUsernameWhenDataIsNotPresent() {
+        Cart cart = new Cart();
+        cart.setId(1L);
+        cart.setUsername("mockusername");
+        cart.setDiscount(BigDecimal.ZERO);
+        cart.setTotalDiscount(BigDecimal.ZERO);
+        cart.setPromotionCodes("promo-code");
+        cart.setSubtotal(BigDecimal.ZERO);
+        cart.setGrandTotal(BigDecimal.ZERO);
+
+        when(cartRepository.getCartByUsername(anyString())).thenReturn(Optional.empty());
+        when(cartRepository.save(any())).thenReturn(cart);
+
+        cartItemService.getCartByUsername("username");
+        verify(cartRepository, times(1)).save(any());
     }
 }
